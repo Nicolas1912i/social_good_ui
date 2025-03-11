5264cee987095e676543460fd97d490de5cf143e
Nicolas Arias <anonymmajora@gmail.com>
2025-03-10 23:06:33 -0500

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,47 @@
-import type { Config } from "tailwindcss";
+import type {
+	Config
+} from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	darkMode: ["class"],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				main: 'var(--main)',
+				overlay: 'var(--overlay)',
+				bg: 'var(--bg)',
+				bw: 'var(--bw)',
+				blank: 'var(--blank)',
+				text: 'var(--text)',
+				mtext: 'var(--mtext)',
+				border: 'var(--border)',
+				ring: 'var(--ring)',
+				ringOffset: 'var(--ring-offset)',
+
+				secondaryBlack: '#212121',
+			},
+			borderRadius: {
+				base: '10px'
+			},
+			boxShadow: {
+				shadow: 'var(--shadow)'
+			},
+			translate: {
+				boxShadowX: '6px',
+				boxShadowY: '6px',
+				reverseBoxShadowX: '-6px',
+				reverseBoxShadowY: '-6px',
+			},
+			fontWeight: {
+				base: '500',
+				heading: '700',
+			},
+		},
+	},
+	plugins: [require("tailwindcss-animate")],
 } satisfies Config;
